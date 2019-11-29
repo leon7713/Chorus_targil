d8a5423ae33ccef41bbb944b6350d7588834b862
leon7713 <leonushar@gmail.com>
2019-11-29 18:32:36 +0200

    Second commit

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -1,29 +1,20 @@
-from selenium.webdriver.common.by import By
 from base.selenium_driver import SeleniumDriver
+import utilities.custom_logger as cl
+import logging
+
 
 class LoginPage(SeleniumDriver):
+    log = cl.customLogger(logging.DEBUG)
 
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
 
-    #Locators
+    # Locators
     _login_link = "Login"
     _email_field = "user_email"
     _password_field = "user_password"
-    _login_button = "commit"
-
-    # def getLoginLink(self):
-    #     return self.driver.find_element(By.LINK_TEXT, self._login_link)
-    #
-    # def getEmailField(self):
-    #     return self.driver.find_element(By.ID, self._email_field)
-    #
-    # def getPasswordField(self):
-    #     return self.driver.find_element(By.ID, self._password_field)
-    #
-    # def getLoginButton(self):
-    #     return self.driver.find_element(By.NAME, self._login_button)
+    _login_button = "[name = commit]"
 
     def clickLoginLink(self):
         self.elementClick(self._login_link, locatorType="link")
@@ -35,10 +26,18 @@ class LoginPage(SeleniumDriver):
         self.sendKeys(password, self._password_field, locatorType="id")
 
     def clickOnLoginButton(self):
-        self.elementClick(self._login_button, locatorType="name")
+        self.elementClick(self._login_button, locatorType="css")
 
-    def login(self, email, password):
+    def login(self, email="", password=""):
         self.clickLoginLink()
         self.enterEmail(email)
         self.enterPassword(password)
         self.clickOnLoginButton()
+
+    def verifyLoginSuccessful(self):
+        result = self.isElementPresent("gravatar", locatorType="class")
+        return result
+
+    def verifyLoginFailed(self):
+        result = self.isElementPresent(".alert.alert-danger", locatorType="css")
+        return result
--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -1,29 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import os
 from pages.home.login_page import LoginPage
 import unittest
+import pytest
 
+@pytest.mark.usefixtures("oneTimeSetUp", "setUp")
 class LoginTests(unittest.TestCase):
 
+    @pytest.fixture(autouse=True)
+    def classSetup(self, oneTimeSetUp):
+        self.lp = LoginPage(self.driver)
+
+    @pytest.mark.run(order=2)
     def test_validLogin(self):
-        driverLocation = "C:\\Users\\Leonidus\\IdeaProjects\\testselenium\\drivers\\chromedriver.exe"
-        os.environ["webdriver.chrome.driver"] = driverLocation
-        baseURL = "https://learn.letskodeit.com/"
-        driver = webdriver.Chrome(driverLocation)
-        driver.maximize_window()
-        driver.implicitly_wait(3)
-        driver.get(baseURL)
+        self.lp.login("test@email.com", "abcabc")
+        result = self.lp.verifyLoginSuccessful()
+        assert result == True
 
-        lp = LoginPage(driver)
-        lp.login("test@email.com", "abcabc")
+    @pytest.mark.run(order=1)
+    def test_invalidLogin(self):
+        self.lp.login("", "abcabc7")
+        result = self.lp.verifyLoginFailed()
+        assert result == True
 
-
-        avatarIcon = driver.find_element(By.CLASS_NAME, "gravatar")
-        if avatarIcon is not None:
-            print("Login Successful")
-        else:
-            print("Login failed")
-
-ff = LoginTests()
-ff.test_validLogin()
